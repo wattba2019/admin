2caf7e378bb1c04863f52d353f059f97135bd552
Abdullah <abddullahshah@gmail.com>
2020-02-19 14:42:30 +0500

    config url change and link heroku with github

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,11 @@
 var baseURL = {
-    // baseURL: "https://fathomless-citadel-43321.herokuapp.com",
+    baseURL: "https://fathomless-citadel-43321.herokuapp.com",
     // baseURL: "http://192.168.200.130:3002",
-    baseURL: "https://fierce-stream-75371.herokuapp.com",
+    // baseURL: "https://fierce-stream-75371.herokuapp.com",
     // baseURL: "http://localhost:3002"
 }
 
 
 export default baseURL;
+
+// http://localhost:3002/bookings/5dfb488f662af31be47f3254/Sat Feb 08 2020 00:00:00 GMT+0500 (Pakistan Standard Time)
